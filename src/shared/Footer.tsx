2e0c5db20ca9d6f09e06d8f764a862668ea5062d
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-18 09:47:07 +0900

    style: Footer UI 구현 #4

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,5 +1,28 @@
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="bg-[#ACDACD] text-black px-4 py-3 flex justify-between items-center h-[120px]">
+      <div className="flex items-center ml-6">
+        <img src="/logo.png" alt="로고" className="h-10" />
+        <div className="ml-5 font-bold text-sm">2024 Dopamine, Team.</div>
+      </div>
+
+      <div className="flex items-center space-x-4 relative mr-6">
+        <a
+          className="hover:text-[#2E9093]"
+          href="https://github.com/web-team-dopamine"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dopamine, Team’s GitHub
+        </a>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Github-desktop-logo-symbol.svg/2048px-Github-desktop-logo-symbol.svg.png"
+          className="w-10 h-10"
+          alt="github"
+        />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
