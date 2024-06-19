35c8f0353f3e6933a6696c00ecbbd512114f0072
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-19 09:41:19 +0900

    style: userTypeSelection UI #5

---
+++ src/components/Join/UserTypeSelection.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const UserTypeSelection = () => {
+  return (
+    <>
+      <label
+        htmlFor="usertype"
+        className="block text-gray-700 text-m font-bold mb-2"
+      >
+        이메일<span className="text-red-600">*</span>
+      </label>
+      <div className="flex">
+        <div className="flex items-center mb-2">
+          <input
+            className="appearance-none w-3 h-3 mr-2 border border-gray-300 rounded-full checked:bg-[#2E9093] focus:outline-none"
+            type="radio"
+            name="userType"
+            id="radioSeller"
+          />
+          <label className="cursor-pointer text-sm mr-6" htmlFor="radioSeller">
+            판매자
+          </label>
+        </div>
+        <div className="flex items-center mb-2">
+          <input
+            className="appearance-none w-3 h-3 mr-2 border border-gray-300 rounded-full checked:bg-[#2E9093] focus:outline-none"
+            type="radio"
+            name="userType"
+            id="radioBuyer"
+            defaultChecked
+          />
+          <label className="cursor-pointer text-sm" htmlFor="radioBuyer">
+            구매자
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserTypeSelection;
