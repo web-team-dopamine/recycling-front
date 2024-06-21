80e0dd7983c2200a3b1b86ebe6a0eba0f4ded893
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-21 17:49:42 +0900

    style: 좋아요, 장바구니, 북마크 기능 추가 #7

--- src/components/Main/ProductItem.tsx
+++ src/components/Main/ProductItem.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaHeart, FaShoppingCart } from 'react-icons/fa'; // Importing icons for wishlist and cart
 import { useNavigate } from 'react-router-dom';
+import useModal from '../../hooks/useModal';
 
 const ProductItem = ({ image, title, company, price }: ProductItemProps) => {
   const navigate = useNavigate();
   const [hovered, setHovered] = useState(false);
 
+  // 좋아요, 장바구니 추가 관련 modal
+  const { openModal: openMarkModal, Modal: MarkModal } = useModal();
+  const { openModal: openCartModal, Modal: CartModal } = useModal();
+
+  // 임의로 login 했다고 가정
+  const isLoggedIn = () => {
+    return true;
+  };
+
   return (
     <div
       className="max-w-xs rounded overflow-hidden shadow-lg bg-white m-4 relative cursor-pointer"
-      onClick={() => navigate('/product/:id')}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
@@ -18,17 +27,23 @@ const ProductItem = ({ image, title, company, price }: ProductItemProps) => {
         {hovered && (
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300">
             <div className="text-white text-center">
-              <button className="text-gray-400 hover:text-red-600 mx-2">
+              <button
+                className="text-gray-400 hover:text-red-600 mx-2"
+                onClick={openMarkModal}
+              >
                 <FaHeart className="inline-block" />
               </button>
-              <button className="text-gray-400 hover:text-[#2E9093] mx-2">
+              <button
+                className="text-gray-400 hover:text-[#2E9093] mx-2"
+                onClick={openCartModal}
+              >
                 <FaShoppingCart className="inline-block" />
               </button>
             </div>
           </div>
         )}
       </div>
-      <div className="px-4 py-4">
+      <div className="px-4 py-4" onClick={() => navigate('/product/:id')}>
         <div
           className={`font-bold text-l mb-2 ${hovered ? 'hover:underline' : ''}`}
         >
@@ -37,6 +52,64 @@ const ProductItem = ({ image, title, company, price }: ProductItemProps) => {
         <p className={`text-gray-700 text-sm mb-2 `}>{company}</p>
         <p className={`text-gray-700 text-base mb-2 `}>{price}</p>
       </div>
+
+      {/* 찜 버튼 클릭 modal */}
+      <MarkModal>
+        {isLoggedIn() ? (
+          <>
+            <div className="text-gray-800 mb-4 text-l">
+              찜에 추가되었습니다!
+              <br />
+              <button
+                onClick={() => navigate('/user/:id/bookmark')}
+                className="text-[#2E9093] hover:text-opacity-60 underline"
+              >
+                [마이페이지 &gt; 찜 보러가기]
+              </button>
+              에서 찾을 수 있습니다.
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="text-gray-800 mb-4">로그인이 필요합니다.</div>
+            <button
+              className="bg-[#2E9093] text-white px-4 py-2 rounded hover:bg-opacity-80 mr-2"
+              onClick={() => navigate('/login')}
+            >
+              로그인하기
+            </button>
+          </>
+        )}
+      </MarkModal>
+
+      {/* 장바구니 클릭 modal */}
+      <CartModal>
+        {isLoggedIn() ? (
+          <>
+            <div className="text-gray-800 mb-4 text-l">
+              장바구니에 추가되었습니다!
+              <br />
+              <button
+                onClick={() => navigate('/user/:id/basket')}
+                className="text-[#2E9093] hover:text-opacity-60 underline"
+              >
+                [장바구니 보러가기]
+              </button>
+              에서 찾을 수 있습니다.
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="text-gray-800 mb-4">로그인이 필요합니다.</div>
+            <button
+              className="bg-[#2E9093] text-white px-4 py-2 rounded hover:bg-opacity-80 mr-2"
+              onClick={() => navigate('/login')}
+            >
+              로그인 하기
+            </button>
+          </>
+        )}
+      </CartModal>
     </div>
   );
 };
--- src/components/Main/ProtectionItem.tsx
+++ src/components/Main/ProtectionItem.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BiLike } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
+import useModal from '../../hooks/useModal';
 
 const ProtectionItem = ({
   image,
@@ -11,6 +12,28 @@ const ProtectionItem = ({
 }: ProtectionItemProps) => {
   const navigate = useNavigate();
   const [hovered, setHovered] = useState(false);
+  const [liked, setLiked] = useState(false); // 좋아요 클릭 여부
+
+  const {
+    openModal: openLikeModal,
+    closeModal: closeLikeModal,
+    Modal: LikeModal,
+  } = useModal();
+
+  // 임의로 로그인했다고 표시
+  const isLoggedIn = () => {
+    return true;
+  };
+
+  // 좋아요 클릭
+  const handleLikeClick = () => {
+    if (isLoggedIn()) {
+      setLiked(true);
+      closeLikeModal();
+    } else {
+      openLikeModal();
+    }
+  };
 
   return (
     <div
@@ -23,23 +46,50 @@ const ProtectionItem = ({
         {hovered && (
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300">
             <div className="text-white text-center">
-              <button className="text-gray-400 hover:text-[#2E9093] mr-1">
-                <BiLike className="inline-block" />
+              <button
+                className={`text-gray-400 hover:text-gray-500 mr-1 `}
+                onClick={handleLikeClick}
+              >
+                <BiLike
+                  className={`inline-block ${liked ? 'text-[#2E9093]' : ''} `}
+                />
               </button>
               {likes}
             </div>
           </div>
         )}
       </div>
-      <div className="px-4 py-4" onClick={() => navigate('/protection/:id')}>
+      <div
+        className="px-4 py-4 cursor-pointer"
+        onClick={() => navigate('/protection/:id')}
+      >
         <div
-          className={`font-bold text-l mb-2 ${hovered ? 'hover:underline' : ''}`}
+          className={`font-bold text-l mb-2 ${
+            hovered ? 'hover:underline' : ''
+          }`}
         >
           {title}
         </div>
-        <p className={`text-gray-700 text-sm mb-2 `}>{nickname}</p>
-        <p className={`text-gray-700 text-base mb-2 `}>{date}</p>
+        <p className="text-gray-700 text-sm mb-2 ">{nickname}</p>
+        <p className="text-gray-700 text-base mb-2 ">{date}</p>
       </div>
+
+      {/* 좋아요 버튼 클릭 modal */}
+      <LikeModal>
+        {isLoggedIn() ? (
+          <></> // No content when logged in and liked
+        ) : (
+          <>
+            <div className="text-gray-800 mb-4">로그인이 필요합니다.</div>
+            <button
+              className="bg-[#2E9093] text-white px-4 py-2 rounded hover:bg-opacity-80 mr-2"
+              onClick={() => navigate('/login')}
+            >
+              로그인 하기
+            </button>
+          </>
+        )}
+      </LikeModal>
     </div>
   );
 };
