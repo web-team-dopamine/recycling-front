f13c3c769254846235639d851d12561e92b9449d
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-24 15:58:15 +0900

    chore: 폴더 구조 변경 #10
    - 제품 메인 사이트와 업체 메인 사이트에서 공통으로 필요한 컴포넌트를 Common 폴더로 이동했습니다

---
+++ src/components/Common/index.ts
@@ -0,0 +1,4 @@
+import Banner from './Banner';
+import PostButton from './PostButton';
+
+export { Banner, PostButton };
--- src/components/ProductMain/index.ts
+++
@@ -1,5 +0,0 @@
-import Banner from './Banner';
-import PostButton from './PostButton';
-import ItemList from './ItemList';
-
-export { Banner, PostButton, ItemList };
