2061c851fbbe5b7e8cd9a8e33b7d6127f74e8a29
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-25 16:09:38 +0900

    style: 상품 상세페이지 UI #8

---
+++ src/components/ProductDetail/Tab/index.ts
@@ -0,0 +1,5 @@
+import DetailTab from './DetailTab';
+import CompanyTab from './CompanyTab';
+import ReviewTab from './ReviewTab';
+
+export { DetailTab, CompanyTab, ReviewTab };
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { DetailContent, MainContent } from '../components/ProductDetail';
 
 const ProductDetail = () => {
-  return <div>홍보글 디테일 페이지</div>;
+  return (
+    <div>
+      {/* 제품 설명 */}
+      <div>
+        <MainContent />
+      </div>
+
+      {/* 제품 상세 설명 (상세 정보/리뷰/업체 다른 상품) */}
+      <div className="mb-6">
+        <DetailContent />
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
