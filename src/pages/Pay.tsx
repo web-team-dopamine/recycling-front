dd630280dccb4ca68c0306f0e6e6902e8093d381
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-28 11:13:07 +0900

    style: 결제 페이지 UI #15

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import { PayTitle } from '../components/Common';
+import { Buttons, DeliveryInfo, OrderProducts } from '../components/Pay';
 
 const Pay = () => {
-  return <div>결제 페이지</div>;
+  return (
+    <div className="max-w-screen-lg mx-auto mt-6 mb-16">
+      <PayTitle section2="#2E9093" />
+
+      <div className="border-b mb-4"></div>
+
+      {/* 배송지 */}
+      <DeliveryInfo />
+
+      <div className="border-b  mb-4"></div>
+
+      {/* 주문 상품 내역 */}
+      <OrderProducts />
+
+      {/* 결제, 취소 버튼 */}
+      <Buttons />
+    </div>
+  );
 };
 
 export default Pay;
