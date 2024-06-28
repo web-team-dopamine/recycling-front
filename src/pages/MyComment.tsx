963fa6e9b55abd6e8a634080dd4585f1b6990111
seoyeon-jung <jungluchia1204@gmail.com>
2024-06-28 17:46:42 +0900

    style: 내가 작성한 리뷰 리스트 #19

--- src/pages/MyComment.tsx
+++ src/pages/MyComment.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React from 'react';
+import CommentList from '../components/MyComment/CommentList';
 
 const MyComment = () => {
-  return <div>my comment page</div>;
+  const reviews = [
+    {
+      id: 1,
+      productName: 'Product A',
+      companyName: 'Company X',
+      date: '2024-06-28',
+    },
+    {
+      id: 2,
+      productName: 'Product B',
+      companyName: 'Company Y',
+      date: '2024-06-27',
+    },
+    {
+      id: 3,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 4,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 5,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 6,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 7,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 8,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 9,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 10,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 11,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+    {
+      id: 12,
+      productName: 'Product C',
+      companyName: 'Company Z',
+      date: '2024-06-26',
+    },
+  ];
+
+  return (
+    <div className="max-w-screen-lg mx-auto mt-6 mb-16">
+      <CommentList reviews={reviews} />
+    </div>
+  );
 };
 
 export default MyComment;
